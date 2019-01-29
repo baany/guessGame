be8620ed38000eab0c9a66607c04aac35077c55c
Vivek Banerjee <vb.nitd@gmail.com>
2019-01-29 12:41:48 +0530

    Commit on 29-01-2018 13:03

--- creatingMovieDict.py
+++ creatingMovieDict.py
@@ -1,4 +1,13 @@
 from tinydb import TinyDB, Query
 
 db = TinyDB('db.json')
-db.insert({'movies':{1:'Jajantram Mamantram',2:'Mughal-e-Azam',3:'Dil to Pagal Hai',4:'Aashiqui'}})
+#db.insert({'movies':{1:'Jajantram Mamantram',2:'Mughal-e-Azam',3:'Dil to Pagal Hai',4:'Aashiqui'}})
+moviesDict = db.all()[0]['movies']
+print (moviesDict)
+leng = len(moviesDict)
+#print (leng)
+movie2add = input("Enter the movie name : ")
+moviesDict.update({leng+1:movie2add})
+db.purge()
+db.insert({'movies':moviesDict})
+print (db.all())
